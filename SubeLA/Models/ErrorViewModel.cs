2b00e42f5788fda7c31e77a48b2f6eb9b27fef4c
Laly Durón <LalyDOsorto@gmail.com>
2022-02-26 20:48:12 -0600

    First Commit

---
+++ SubeLA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SubeLA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
